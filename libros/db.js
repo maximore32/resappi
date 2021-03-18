0b2075b911c967ea9204c55e7665e94c2c7e7f82
maximore32 <78425799+maximore32@users.noreply.github.com>
2021-03-18 11:12:34 -0300

    gitmong

--- libros/db.js
+++ libros/db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-const conexion = process.env.MONGOURL || 'mongodb+srv://maxpower:helloworld@cluster0.6di0t.mongodb.net/testmax1?retryWrites=true&w=majority';
+const conexion = process.env.MONGOURL;
 mongoose.connect(conexion ,{useNewUrlParser: true, useUnifiedTopology: true});
