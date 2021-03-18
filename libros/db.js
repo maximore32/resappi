536cd537d42f1ba5c8d7518d5cb010378d65bbd2
maximore32 <78425799+maximore32@users.noreply.github.com>
2021-03-18 10:35:56 -0300

    mongo

--- libros/db.js
+++ libros/db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-const conexion = 'mongodb+srv://maxpower:helloworld@cluster0.6di0t.mongodb.net/testmax1?retryWrites=true&w=majority';
+const conexion = process.env.MONGOURL || 'mongodb+srv://maxpower:helloworld@cluster0.6di0t.mongodb.net/testmax1?retryWrites=true&w=majority';
 mongoose.connect(conexion ,{useNewUrlParser: true, useUnifiedTopology: true});
