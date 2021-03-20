6515d56f2b53cead1b6c6ff1fe7ea492a291d98c
maximore32 <78425799+maximore32@users.noreply.github.com>
2021-03-20 11:25:23 -0300

    change1111

--- libros/index.js
+++ libros/index.js
@@ -2,9 +2,17 @@ var express = require('express');
 var app = express();
 // Importamos la conexión a la base de datos
 var db = require('./db');
-
 // El archivo que tiene todo el armado de la Rest API
 var moviesController = require('./books/Bookscontrols');
+
+//Para react |  Es un build de un proyecto React
+const path = require("path");
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+ });
 
 // La Rest API queda en /api/
 app.use('/api', moviesController);
